ebda8c6bba587727aa33a6ff0bb13921122eb8e6
marcusoftnet <marcusoft.net@gmail.com>
2013-07-16 20:47:11 +0200

    Remove the packages folder from git

--- packages/SuperDuperHappyPath.0.3.0/tools/init.ps1
+++
@@ -1,6 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-Import-Module (Join-Path $toolsPath SuperDuperHappyPath.psd1)
-
-Write-Host "Commands Added:"
-Write-Host "SDHP Hosting ViewEngine Database"
-Write-Host "(Tab completion is available for all parameters)"
