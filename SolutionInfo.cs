caf1153f95007c1aba9b1c2dc988b346733f6594
marcusoftnet <marcusoft.net@gmail.com>
2013-07-16 20:22:23 +0200

    Updated with NuGet restore and also the SolutionInfo.cs construct

---
+++ SolutionInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Marcusoftnet (www.marcusoft.net")]
+[assembly: AssemblyProduct("AbbeSays")]
+[assembly: AssemblyCopyright("Copyright Marcusoftnet © 2013")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.1.*")]
